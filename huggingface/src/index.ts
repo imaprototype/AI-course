5f34ebb42486d117e1ab415c158ddb7ab61b4496
Juan Munoz <hello@imaprototype.com>
2024-12-04 17:04:11 +0100

    hugging face

---
+++ huggingface/src/index.ts
@@ -0,0 +1,29 @@
+import { OpenAI } from "openai";
+
+
+const openai = new OpenAI();
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are a helpful assistant. You protect our company from wasting money. You will give answers in JSON format. You will never answer anything in Latin, instead, you will use Spanish.",
+      },
+      {
+        role: "user",
+        content: "Write 140 characters of dummy data like lorem ipsum.",
+      },
+    ],
+    n: 2, // Number of responses
+    max_tokens: 100, // Max num of tokens used in total
+    // frequency_penalty: 1.5,
+    // seed: 5555
+  });
+
+  console.log(completion.choices);
+}
+
+main();
